7a245cfedeea60d0814b536b69801ce3adf0be27
Luca Abbati <luca.abbati@datadoghq.com>
2024-06-12 15:00:19 +0200

    Rum+nodejs+dotnet working

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+Prepare the agent
+
+```
+docker compose up
+```
+
 Node.js
 
 ```
@@ -5,8 +11,10 @@ npm run start
 ```
 
 
-.NET Core
+.NET app (on Mac M1)
 
 ```
 CORECLR_ENABLE_PROFILING=1 CORECLR_PROFILER={846F5F1C-F9AE-4B07-969E-05C26BC060D8} CORECLR_PROFILER_PATH="$(pwd)/bin/Debug/net8.0/datadog/osx/Datadog.Trace.ClrProfiler.Native.dylib" DD_DOTNET_TRACER_HOME="$(pwd)/bin/Debug/net8.0/datadog" dotnet run
 ```
+
+Browser: go to [http://localhost:3000]
--- app.js
+++ app.js
@@ -4,6 +4,57 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
+    var content = `
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>HTML5 Boilerplate</title>
+        <script
+            src="https://www.datadoghq-browser-agent.com/us1/v5/datadog-rum.js"
+            type="text/javascript">
+        </script>
+        <script>
+            window.DD_RUM && window.DD_RUM.init({
+            clientToken: 'pub080bc2d58b329f573ba8dff4d642b138',
+            applicationId: '7c732f70-d533-4372-83cb-6e2d5f951b6b',
+            site: 'datadoghq.com',
+            service: 'labbati-rum-tests',
+            env: 'labbati-local',
+            // Specify a version number to identify the deployed version of your application in Datadog
+            // version: '1.0.0',
+            allowedTracingUrls: ["http://localhost:3000"],
+            sessionSampleRate: 100,
+            sessionReplaySampleRate: 20,
+            trackUserInteractions: true,
+            trackResources: true,
+            trackLongTasks: true,
+            defaultPrivacyLevel: 'mask-user-input',
+            });
+
+            function sendAjax() {
+                console.log('Sending ajax');
+                let xhttp = new XMLHttpRequest();
+                xhttp.open("GET", "/ajax", true);
+                xhttp.send();
+                console.log('Done');
+            }
+        </script>
+    </head>
+
+    <body>
+        <h1>Page Title</h1>
+        <p>Hello!</p>
+        <button onclick="sendAjax()">Send</button>
+    </body>
+    </html>
+    `;
+    res.send(content);
+})
+
+app.get('/ajax', (req, res) => {
     axios.get('http://localhost:5163')
         .then(function (response) {
             // handle success
@@ -17,8 +68,7 @@ app.get('/', (req, res) => {
             // always executed
             // res.send('Hello World!')
         });
-
-})
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
