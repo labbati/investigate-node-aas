3560ea3b078ec2febd0faea15968f833c26b3a66
Luca Abbati <luca.abbati@datadoghq.com>
2024-06-21 17:46:47 +0200

    Remove all the promises

--- app.js
+++ app.js
@@ -72,9 +72,9 @@ app.get('/ajax', (req, res) => {
 
 
 (async () => {
-    process._rawDebug('about to wait a very long time')
-    for (let i = 0; i < 10000; i++) await new Promise(r => r())
-    process._rawDebug('done waiting a very long time')
+    // process._rawDebug('about to wait a very long time')
+    // for (let i = 0; i < 10000; i++) await new Promise(r => r())
+    // process._rawDebug('done waiting a very long time')
     app.listen(port, () => {
         console.log(`Example app listening on port ${port}`)
     })
