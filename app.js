f5f0ed1eb88157444ad9fbef30d4efdbdf434d13
Luca Abbati <luca.abbati@datadoghq.com>
2024-08-27 12:25:04 +0200

    Make port configurable and add a log statement for debugging purposes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const axios = require('axios');
 const app = express()
-const port = 8080
+const port = process.env.PORT || 8080
 
 const BUILD = '0000003';
 
@@ -10,6 +10,7 @@ function directLog(message) {
 }
 
 app.get('/', (req, res) => {
+    process._rawDebug(`This is a raw log`);
     res.send('OK');
 });
 
